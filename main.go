3a7996ee6321d2eec0b3ed41f1949d87ec5b910c
geralt18 <geralt18@gmail.com>
2021-03-26 15:43:16 +0100

    Read manga urls from text file

--- main.go
+++ main.go
@@ -1,28 +1,53 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"io"
+	"log"
 	"net/http"
 	"os"
+	"strings"
 )
 
 func main() {
-	urls := map[string]string{
-		//"https://official-complete-1.granpulse.us/manga/Akira/":      "D:\\Temp\\!Manga\\Akira\\",
-		"https://scans-complete.hydaelyn.us/manga/Gantz/":            "D:\\Temp\\!Manga\\Gantz\\",
-		"https://official-complete-2.eorzea.us/manga/Toukyou-Kushu/": "D:\\Temp\\!Manga\\Tokyo Ghoul\\",
-		"https://official-complete-1.granpulse.us/manga/Uzumaki/":    "D:\\Temp\\!Manga\\Uzumaki\\",
+	file, err := os.Open("mangas.txt")
+	if err != nil {
+		log.Fatal(err)
+		os.Exit(3)
 	}
+	defer file.Close()
+
+	urls := make(map[string]string)
+	// urls := map[string]string{
+	// 	//"https://official-complete-1.granpulse.us/manga/Akira/":      "D:\\Temp\\!Manga\\Akira\\",
+	// 	"https://scans-complete.hydaelyn.us/manga/Gantz/":            "D:\\Temp\\!Manga\\Gantz\\",
+	// 	"https://official-complete-2.eorzea.us/manga/Toukyou-Kushu/": "D:\\Temp\\!Manga\\Tokyo Ghoul\\",
+	// 	"https://official-complete-1.granpulse.us/manga/Uzumaki/":    "D:\\Temp\\!Manga\\Uzumaki\\",
+	// }
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		s := strings.Split(scanner.Text(), ";")
+		urls[strings.TrimSpace(s[0])] = strings.TrimSpace(s[1])
+	}
+
 	for k, v := range urls {
 		baseUrl := k
 		basePath := v
-		chapterCount := 5
+		chapterStart := 5
+		chapterCount := 1
 
-		for chapter := 1; chapter < chapterCount; chapter++ {
-			fmt.Printf("Rozpoczynam pobieranie rodziału %04d\n", chapter)
-			os.MkdirAll(fmt.Sprintf("%s%04d", basePath, chapter), 0777)
+		for chapter := chapterStart; chapter < chapterStart+chapterCount; chapter++ {
 			page := 0
+			dirPath := fmt.Sprintf("%s%04d", basePath, chapter)
+
+			fmt.Printf("Rozpoczynam pobieranie rodziału %04d\n", chapter)
+			err := os.MkdirAll(dirPath, 0777)
+			if err != nil {
+				fmt.Println("Problem z utworzeniem katalogu ", dirPath, err)
+				break
+			}
 
 			for {
 				page++
