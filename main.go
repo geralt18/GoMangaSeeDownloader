e8e68f63bdd1c0498a13be4475e28b7df28a202e
geralt18 <geralt18@gmail.com>
2021-03-30 11:18:01 +0200

    Add comand line arguments

--- main.go
+++ main.go
@@ -6,32 +6,52 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"net/url"
 	"os"
+	"path"
+	"path/filepath"
 	"strings"
 	"sync"
 )
 
 func main() {
-	file, err := os.Open("mangas.txt")
-	if err != nil {
-		log.Fatal(err)
-		os.Exit(3)
-	}
-	defer file.Close()
-
 	urls := make(map[string]string)
+	args := os.Args[1:]
+
+	if len(args) > 1 {
+		if filepath.IsAbs(strings.TrimSpace(args[1])) {
+			urls[strings.TrimSpace(args[0])] = strings.TrimSpace(args[1])
+		} else {
+			urls[strings.TrimSpace(args[0])] = path.Join(GetExeDirectory(), strings.TrimSpace(args[1]))
+		}
+	} else if len(args) == 1 {
+		u, err := url.Parse(args[0])
+		if err != nil {
+			log.Fatal(err)
+			os.Exit(1)
+		}
+		urls[strings.TrimSpace(args[0])] = path.Join(GetExeDirectory(), strings.TrimSpace(u.Path))
+	} else {
+		file, err := os.Open("mangas.txt")
+		if err != nil {
+			log.Fatal(err)
+			os.Exit(3)
+		}
+		defer file.Close()
+
+		scanner := bufio.NewScanner(file)
+		for scanner.Scan() {
+			s := strings.Split(scanner.Text(), ";")
+			urls[strings.TrimSpace(s[0])] = strings.TrimSpace(s[1])
+		}
+	}
+
 	// urls := map[string]string{
-	// 	//"https://official-complete-1.granpulse.us/manga/Akira/":      "D:\\Temp\\!Manga\\Akira\\",
+	// 	"https://official-complete-1.granpulse.us/manga/Akira/":      "D:\\Temp\\!Manga\\Akira\\",
 	// 	"https://scans-complete.hydaelyn.us/manga/Gantz/":            "D:\\Temp\\!Manga\\Gantz\\",
 	// 	"https://official-complete-2.eorzea.us/manga/Toukyou-Kushu/": "D:\\Temp\\!Manga\\Tokyo Ghoul\\",
 	// 	"https://official-complete-1.granpulse.us/manga/Uzumaki/":    "D:\\Temp\\!Manga\\Uzumaki\\",
 	// }
-
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		s := strings.Split(scanner.Text(), ";")
-		urls[strings.TrimSpace(s[0])] = strings.TrimSpace(s[1])
-	}
 
 	var wg sync.WaitGroup
 
@@ -47,12 +67,23 @@ func main() {
 	wg.Wait()
 }
 
+func GetExeDirectory() string {
+	ex, err := os.Executable()
+	if err != nil {
+		log.Fatal(err)
+		os.Exit(10)
+	}
+	return filepath.Dir(ex)
+}
+
 func DownloadManga(chapterStart int, chapterCount int, basePath string, baseUrl string, wg *sync.WaitGroup) {
 	defer wg.Done()
+	fmt.Printf("Manga url: %s\n", baseUrl)
+	fmt.Printf("Folder path: %s\n", basePath)
 
 	for chapter := chapterStart; chapter < chapterStart+chapterCount; chapter++ {
 		page := 0
-		dirPath := fmt.Sprintf("%s%04d", basePath, chapter)
+		dirPath := path.Join(basePath, fmt.Sprintf("%04d", chapter))
 
 		fmt.Printf("Rozpoczynam pobieranie rodziału %04d\n", chapter)
 		err := os.MkdirAll(dirPath, 0777)
@@ -64,7 +95,7 @@ func DownloadManga(chapterStart int, chapterCount int, basePath string, baseUrl 
 		for {
 			page++
 			url := fmt.Sprintf("%s%04d-%03d.png", baseUrl, chapter, page)
-			filePath := fmt.Sprintf("%s%04d\\%03d.png", basePath, chapter, page)
+			filePath := path.Join(basePath, fmt.Sprintf("%04d\\%03d.png", chapter, page))
 
 			fmt.Printf("Pobieram plik %s\n", url)
 			err := DownloadFile(filePath, url)
