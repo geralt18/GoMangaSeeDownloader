3fd877ac2462dd7d68c5faffc9d909904111d2c4
geralt18 <geralt18@gmail.com>
2021-03-31 14:21:27 +0200

    Better directory structure

--- build.ps1
+++ build.ps1
@@ -2,4 +2,4 @@ $now = Get-Date -UFormat "%Y-%m-%d_%T"
 $ver = "0.1.0"
 $filePath = "bin/mangasee-$ver.exe" 
 
-go build -o $filePath -ldflags "-X main.Version=$ver -X main.BuildTime=$now"
+go build -o $filePath -ldflags "-X main.Version=$ver -X main.BuildTime=$now" .\src\main.go
