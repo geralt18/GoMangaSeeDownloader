630b4e9cc481f1255714dd80a9f57b56041e2532
geralt18 <geralt18@gmail.com>
2021-04-01 12:26:04 +0200

    Add zip package

--- build.ps1
+++ build.ps1
@@ -1,5 +1,8 @@
 $now = Get-Date -UFormat "%Y-%m-%d_%T"
 $ver = "0.1.0"
 $filePath = "bin/mangasee-$ver.exe" 
+$zipPath = "bin/mangasee-$ver.zip" 
 
 go build -o $filePath -ldflags "-X main.Version=$ver -X main.BuildTime=$now" main.go
+
+Compress-Archive -Path $filePath -DestinationPath $zipPath
